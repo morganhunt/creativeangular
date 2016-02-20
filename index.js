cdf6064512588897ce93c515a491412e58214828
Morgan Hunt <to.morgan.hunt@gmail.com>
2016-02-20 15:56:43 -0700

    new picture, some functionality, some countries prints to console

--- index.js
+++ index.js
@@ -12,6 +12,54 @@ function mainCtrl($scope){
 			countryArray[Math.floor(Math.random() * (countryArray.length))]
 		);
 	}
+	console.log("call select country");
+	selectCountry();
+}
+
+function selectCountry(){
+	$("#myMap").on("click", function(event){
+		console.log(event.pageY +"px");
+		console.log(event.pageX + "px");
+		var name;
+		if(event.pageX >= 270 && event.pageX <= 380 && event.pageY >=420 && event.pageY <= 445){
+			name = "France";
+			console.log(name);
+		}
+		if(event.pageX >= 130 && event.pageX <= 210 && event.pageY >=550 && event.pageY <= 570){
+			name = "Spain"
+			console.log(name);
+		}
+		if(event.pageX >= 40 && event.pageX <= 115 && event.pageY >=515 && event.pageY <= 595){
+			name = "Portugal"
+			console.log(name);
+		}
+		if(event.pageX >= 235 && event.pageX <= 325 && event.pageY >=315 && event.pageY <= 345){
+			name = "United Kingdom"
+			console.log(name);
+		}
+		if(event.pageX >= 155 && event.pageX <= 215 && event.pageY >=300 && event.pageY <= 320){
+			name = "Ireland"
+			console.log(name);
+		}
+		if(event.pageX >= 175 && event.pageX <= 260 && event.pageY >=90 && event.pageY <= 115){
+			name = "Iceland"
+			console.log(name);
+		}
+		if(event.pageX >= 445 && event.pageX <= 520 && event.pageY >=485 && event.pageY <= 500){
+			name = "Italy"
+			console.log(name);
+		}
+		if(event.pageX >= 440 && event.pageX <= 565 && event.pageY >=345 && event.pageY <= 370){
+			name = "Germany"
+			console.log(name);
+		}
+
+
+
+
+
+
+	})
 }
 
 function countryDirective(){
